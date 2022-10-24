2bf901eb272d651422390beef3662c4ea9b67d29
Axelancerr <axelancerr@gmail.com>
2022-10-24 18:46:59 +0100

    style: restructure test client

--- backend/tests/client.py
+++ backend/tests/client.py
@@ -6,11 +6,11 @@ import aiohttp
 async def main() -> None:
 
     session = aiohttp.ClientSession()
+    headers = {
+        "Authorization": "a"
+    }
 
-    async with session.get(
-        "http://127.0.0.1:8000/api/",
-        headers={"Authorization": "a"}
-    ) as response:
+    async with session.get("http://127.0.0.1:8000/", headers=headers) as response:
         print(await response.json())
 
     await session.close()
