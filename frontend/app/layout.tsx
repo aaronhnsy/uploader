112fcf8f7a0939403fd4c7bab195225b637c7451
Axelancerr <axelancerr@gmail.com>
2023-05-10 23:28:35 +0100

    change logo design

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,6 @@ import { NavBar } from "@/components/navbar"
 import { Metadata } from "next"
 import React from "react"
 
-import "@/styles/bootstrap.scss"
 import "@/styles/global.scss"
 
 export const metadata: Metadata = {
