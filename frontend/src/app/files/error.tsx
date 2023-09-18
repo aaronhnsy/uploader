8c22fdf66fcc44880c2404f633a1b95e7aad5161
Axelancerr <axelancerr@gmail.com>
2023-09-18 01:56:30 +0100

    add error handling for the files page

---
+++ frontend/src/app/files/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+    return (
+        <div>
+            <h1 className="font-bold text-lg text-gray-900 dark:text-gray-100">Something went wrong!</h1>
+        </div>
+    );
+}
--- frontend/src/components/navbar/navbar.login.tsx
+++ frontend/src/components/navbar/navbar.login.tsx
@@ -10,7 +10,7 @@ export function NavbarLogin() {
                   "h-10", "px-2", "rounded",
                   "font-bold", "text-sm", "text-gray-900", "hover:text-gray-950",
                   "bg-accent", "hover:bg-accent-hover",
-                  "focus:outline-none", "focus:ring", "focus:ring-gray-100", "focus:ring-3",
+                  "focus:outline-none", "focus:ring", "focus:ring-gray-900", "dark:focus:ring-gray-100", "focus:ring-3",
                   "theme-transition",
               )}>
             <p>Login</p>
