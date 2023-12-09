a09d0f868fafb22f631ba1a604c3a0df7c96038e
Axelancerr <axelancerr@gmail.com>
2023-12-09 03:06:52 +0000

    use a heart icon to show the current theme and accent colour

--- frontend/src/components/footer/footer.themeButton.tsx
+++ frontend/src/components/footer/footer.themeButton.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { modesAndColours } from "./footer.themeChanger";
 import { clsx } from "clsx";
 import { useTheme } from "next-themes";
 import React, { useEffect, useState } from "react";
+import { modesAndColours } from "./footer.themeChanger";
 
 interface ThemeButtonProps {
     mode?: string;
@@ -11,6 +11,15 @@ interface ThemeButtonProps {
     value: string;
     setValue: React.Dispatch<React.SetStateAction<string>>;
 }
+
+const icon = (
+    <svg className="h-3 w-3"
+         viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+        <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3
+                 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244
+                 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
+    </svg>
+);
 
 export default function ThemeButton({mode, colour, value, setValue}: ThemeButtonProps) {
     // make sure theme is mounted before rendering
@@ -21,9 +30,8 @@ export default function ThemeButton({mode, colour, value, setValue}: ThemeButton
     return (
         <button type="button" aria-label="theme switcher"
                 className={clsx(
-                    mounted && (theme as string).includes(value) ? ["h-5", "w-10"] : ["h-5", "w-5"], "rounded",
+                    "c-button", "h-5", "w-5",
                     modesAndColours[value as keyof typeof modesAndColours],
-                    "focus:scale-[115%]", "hover:scale-[115%]",
                     "focus:outline-none", "ring", "ring-colour-tertiary",
                     "u-transition",
                 )}
@@ -31,6 +39,7 @@ export default function ThemeButton({mode, colour, value, setValue}: ThemeButton
                     setValue(value);
                     setTheme(mode == undefined ? `${value}-${colour}` : `${mode}-${value}`);
                 }}>
+            {mounted && (theme as string).includes(value) ? icon : ""}
         </button>
     );
 }
