4214feb7ee737bcaa2b7988c3a58813538077f18
Aaron Hennessey <aaronhnsy@gmail.com>
2024-02-18 06:17:43 +0000

    redo login form

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,37 +1,43 @@
 import { clsx } from "clsx";
-import { login } from "./login";
 
 export default function Page() {
     return (
-        <div className="flex-1 flex items-center justify-center">
-            <form action={login} className="flex flex-col p-3 space-y-3 bg-colour-primary rounded">
-                <div className="space-y-2">
-                    <label htmlFor="email"
-                           className="block font-bold text-sm text-gray-100">
-                        Email:
+        <div className={clsx("flex-1", "flex", "items-center", "justify-center")}>
+            <form className={clsx(
+                "flex", "flex-col", "grow",
+                "p-4", "max-w-72", "space-y-4",
+                "bg-colour-primary", "rounded",
+            )}>
+                <div className={clsx("flex", "flex-col", "space-y-2")}>
+                    <label className={clsx("font-bold", "text-lg", "text-gray-100")} htmlFor="email">
+                        Email Address
                     </label>
-                    <input type="email" name="email" id="email" placeholder="name@company.com" required
-                           className="p-2 bg-colour-secondary rounded"/>
+                    <input className={clsx(
+                        "p-2",
+                        "font-semibold", "text-md", "text-gray-100",
+                        "bg-colour-secondary", "rounded",
+                    )} id="email" type="email" placeholder="aaron@hnsy.com"/>
                 </div>
-                <div className="space-y-2">
-                    <label htmlFor="password"
-                           className="block font-bold text-sm text-gray-100">
-                        Password:
+                <div className={clsx("flex", "flex-col", "space-y-2")}>
+                    <label className={clsx("font-bold", "text-lg", "text-gray-100")} htmlFor="password">
+                        Password
                     </label>
-                    <input type="password" name="password" id="password" placeholder="••••••••••••••••" required
-                           className="p-2 bg-colour-secondary rounded"/>
+                    <input className={clsx(
+                        "p-2",
+                        "font-semibold", "text-md", "text-gray-100",
+                        "bg-colour-secondary", "rounded",
+                    )} id="password" type="password" placeholder="********"/>
                 </div>
-                <button type="submit" aria-label="login"
-                        className={clsx(
-                            "c-button", "h-10",
-                            "font-bold", "text-md",
-                            "text-gray-900", "hover:text-gray-950",
-                            "bg-colour-accent-primary", "hover:bg-colour-accent-secondary",
-                            "u-ring-primary", "u-transition",
-                        )}>
-                    Login
+                <button className={clsx(
+                    "c-button", "h-10",
+                    "font-bold", "text-sm",
+                    "text-gray-900", "hover:text-gray-950",
+                    "bg-colour-accent-primary", "hover:bg-colour-accent-secondary",
+                    "u-ring-primary", "u-transition",
+                )} type="submit">Login
                 </button>
             </form>
         </div>
+
     );
 }
