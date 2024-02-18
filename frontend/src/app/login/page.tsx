996b11a4fecc7046e7b40eb830a6575c2ab00a62
Aaron Hennessey <aaronhnsy@gmail.com>
2024-02-18 06:24:27 +0000

    change login form text sizing

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -5,33 +5,32 @@ export default function Page() {
         <div className={clsx("flex-1", "flex", "items-center", "justify-center")}>
             <form className={clsx(
                 "flex", "flex-col", "grow",
-                "p-4", "max-w-72", "space-y-4",
+                "p-3", "max-w-96", "space-y-3",
                 "bg-colour-primary", "rounded",
             )}>
                 <div className={clsx("flex", "flex-col", "space-y-2")}>
-                    <label className={clsx("font-bold", "text-lg", "text-gray-100")} htmlFor="email">
+                    <label className={clsx("font-bold", "text-md", "text-gray-100")} htmlFor="email">
                         Email Address
                     </label>
                     <input className={clsx(
                         "p-2",
-                        "font-semibold", "text-md", "text-gray-100",
+                        "font-semibold", "text-sm", "text-gray-100",
                         "bg-colour-secondary", "rounded",
                     )} id="email" type="email" placeholder="aaron@hnsy.com"/>
                 </div>
                 <div className={clsx("flex", "flex-col", "space-y-2")}>
-                    <label className={clsx("font-bold", "text-lg", "text-gray-100")} htmlFor="password">
+                    <label className={clsx("font-bold", "text-md", "text-gray-100")} htmlFor="password">
                         Password
                     </label>
                     <input className={clsx(
                         "p-2",
-                        "font-semibold", "text-md", "text-gray-100",
+                        "font-semibold", "text-sm", "text-gray-100",
                         "bg-colour-secondary", "rounded",
                     )} id="password" type="password" placeholder="********"/>
                 </div>
                 <button className={clsx(
                     "c-button", "h-10",
-                    "font-bold", "text-sm",
-                    "text-gray-900", "hover:text-gray-950",
+                    "font-bold", "text-md", "text-gray-900", "hover:text-gray-950",
                     "bg-colour-accent-primary", "hover:bg-colour-accent-secondary",
                     "u-ring-primary", "u-transition",
                 )} type="submit">Login
--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -11,8 +11,7 @@ export function Navbar() {
                   className={clsx(
                       "c-button", "h-10",
                       "px-3", "space-x-3", "mr-3",
-                      "font-bold", "text-lg",
-                      "text-gray-100", "hover:text-gray-300",
+                      "font-bold", "text-lg", "text-gray-100", "hover:text-gray-300",
                       "bg-colour-primary", "hover:bg-colour-secondary",
                       "fill-colour-accent-primary", "hover:fill-colour-accent-secondary",
                       "u-ring-accent", "u-transition",
@@ -25,8 +24,7 @@ export function Navbar() {
                   className={clsx(
                       "c-button", "h-10",
                       "px-3", "sm:order-1",
-                      "font-bold", "text-sm",
-                      "text-gray-900", "hover:text-gray-950",
+                      "font-bold", "text-sm", "text-gray-900", "hover:text-gray-950",
                       "bg-colour-accent-primary", "hover:bg-colour-accent-secondary",
                       "u-ring-primary", "u-transition",
                   )}>
