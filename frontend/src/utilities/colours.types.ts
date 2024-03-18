7bbafbac2b96345b8e13fbc2006d41005bcc5e98
aaronhnsy <aaronhnsy@gmail.com>
2024-03-18 03:31:34 +0000

    clean-up colour types

--- frontend/src/utilities/colours.types.ts
+++ frontend/src/utilities/colours.types.ts
@@ -1,73 +1,54 @@
 type ModeVariants =
-    `primary` | `primary-hover`
-    | `secondary` | `secondary-hover`
-    | `tertiary` | `tertiary-hover`
-    | `text` | `text-hover`;
+    `primary` | `primary-hover` | `secondary` | `secondary-hover` | `tertiary` | `tertiary-hover` |
+    `text` | `text-hover`;
 type ModeVariantsObject = {
-    [Key in ModeVariants]: string;
+    [MV in ModeVariants]: string
 }
 type Modes = `light` | `dark`;
 export type ModesObject<T extends ModeVariantsObject | AccentVariantsObject = ModeVariantsObject> = {
-    [Key in Modes]: T;
+    [M in Modes]: T
 }
 
 type AccentVariants = `accent` | `accent-hover`;
 type AccentVariantsObject = {
-    [Key in AccentVariants]: string;
+    [AV in AccentVariants]: string
 }
 type Accents = `red` | `orange` | `yellow` | `green` | `blue` | `purple`;
 export type AccentsObject = {
-    [Key in Accents]: ModesObject<AccentVariantsObject>;
+    [A in Accents]: ModesObject<AccentVariantsObject>
 }
 
 type Themes = `${Modes}-${Accents}`;
 
-type ModeOptions = `${Modes}`
-type ModeOptionsClasses<M extends Modes> = `bg-theme-${M}-primary hover:bg-theme-${M}-primary-hover`
+type ModeOptionClasses<M extends Modes> = `bg-theme-${M}-primary hover:bg-theme-${M}-primary-hover`
 export type ModeOptionsObject = {
-    [M in ModeOptions]: ModeOptionsClasses<M>
+    [M in Modes]: ModeOptionClasses<M>
 };
-type AccentOptions = `${Modes}-${Accents}`
-type AccentOptionsClasses<AO extends AccentOptions> = `bg-theme-${AO}-accent hover:bg-theme-${AO}-accent-hover`
+type AccentOptionClasses<T extends Themes> = `bg-theme-${T}-accent hover:bg-theme-${T}-accent-hover`
 export type AccentOptionsObject = {
-    [AO in AccentOptions]: AccentOptionsClasses<AO>
+    [T in Themes]: AccentOptionClasses<T>
 };
 
-type SpecificModeVariantVariables<M extends Modes = Modes, MV extends ModeVariants = ModeVariants> = `--theme-${M}-${MV}`
-type SpecificThemeVariantVariables<T extends Themes = Themes, AV extends AccentVariants = AccentVariants> = `--theme-${T}-${AV}`
-type SpecificVariantVariables = SpecificModeVariantVariables | SpecificThemeVariantVariables
-type RootCSSRulesObject = {
+export type CSSRulesObject = {
     ":root": {
-        [Variant in SpecificVariantVariables]: string;
+        [SV in `--theme-${`${Modes}-${ModeVariants}` | `${Themes}-${AccentVariants}`}`]: string;
+    }
+} & {
+    [M in Modes as `html[data-theme*='${M}']`]: {
+        [MV in ModeVariants as `--theme-${MV}`]: `var(--theme-${M}-${MV})`
+    }
+} & {
+    [T in Themes as `html[data-theme='${T}']`]: {
+        [AV in AccentVariants as `--theme-${AV}`]: `var(--theme-${T}-${AV})`
     }
 }
 
-type ModeSelectors<M extends Modes> = `html[data-theme*='${M}']`;
-type ModeVariantVariables<MV extends ModeVariants> = `--theme-${MV}`
-type ModeCSSRulesObject = {
-    [M in Modes as ModeSelectors<M>]: {
-        [MV in ModeVariants as ModeVariantVariables<MV>]: `var(${SpecificModeVariantVariables<M, MV>})`
-    }
+export type CustomColours = {
+    [SMV in `theme-${Modes}-${ModeVariants}`]: `rgb(var(--${SMV}) / <alpha-value>)`
+} & {
+    [SAV in `theme-${Themes}-${AccentVariants}`]: `rgb(var(--${SAV}) / <alpha-value>)`
+} & {
+    [MV in `theme-${ModeVariants}`]: `rgb(var(--${MV}) / <alpha-value>)`
+} & {
+    [AV in `theme-${AccentVariants}`]: `rgb(var(--${AV}) / <alpha-value>)`
 }
-
-type ThemeSelectors<T extends Themes> = `html[data-theme='${T}']`
-type AccentVariantVariables<AV extends AccentVariants> = `--theme-${AV}`
-type ThemeCSSRulesObject = {
-    [T in Themes as ThemeSelectors<T>]: {
-        [AV in AccentVariants as AccentVariantVariables<AV>]: `var(${SpecificThemeVariantVariables<T, AV>})`
-    }
-}
-
-export type CSSRulesObject = RootCSSRulesObject & ModeCSSRulesObject & ThemeCSSRulesObject;
-
-type ModeVariantCustomColours = {
-    [MV in ModeVariants as `theme-${MV}`]: `rgb(var(${ModeVariantVariables<MV>}) / <alpha-value>)`
-}
-type AccentVariantCustomColours = {
-    [AV in AccentVariants as `theme-${AV}`]: `rgb(var(${AccentVariantVariables<AV>}) / <alpha-value>)`
-}
-type ThemeCustomColours = ModeVariantCustomColours & AccentVariantCustomColours
-
-
-
-export type CustomColours = ThemeCustomColours
