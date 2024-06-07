e7cc5ce851b0e88fdba14d3b75d158cd09900c53
aaronhnsy <aaronhnsy@gmail.com>
2024-06-07 03:00:41 +0100

    fix login page colours

--- frontend/src/components/login/form.login.tsx
+++ frontend/src/components/login/form.login.tsx
@@ -11,37 +11,37 @@ export function LoginForm() {
             <form className={clsx(
                 "flex", "flex-col", "grow",
                 "p-3", "max-w-96",
-                "bg-theme-secondary", "rounded",
+                "bg-colour-secondary", "rounded",
                 "transitions",
             )} action={login}>
                 <div className={clsx("flex", "flex-col", "space-y-2", "mb-3")}>
-                    <label className={clsx("text-size-7", "text-theme-text", "transitions")}
+                    <label className={clsx("text-size-7", "text-colour-text", "transitions")}
                            htmlFor="username">
                         Username
                     </label>
                     <input className={clsx(
                         "p-2", "rounded",
-                        "bg-theme-tertiary",
-                        "text-size-8", "text-theme-text",
+                        "bg-colour-tertiary",
+                        "text-size-8", "text-colour-text",
                         "transitions",
                     )} id="username" type="text" name="username"/>
                 </div>
                 <div className={clsx("flex", "flex-col", "space-y-2", "mb-3")}>
-                    <label className={clsx("text-size-7", "text-theme-text", "transitions")}
+                    <label className={clsx("text-size-7", "text-colour-text", "transitions")}
                            htmlFor="password">
                         Password
                     </label>
                     <input className={clsx(
                         "p-2", "rounded",
-                        "bg-theme-tertiary",
-                        "text-size-8", "text-theme-text",
+                        "bg-colour-tertiary",
+                        "text-size-8", "text-colour-text",
                         "transitions",
                     )} id="password" type="password" name="password"/>
                 </div>
                 <button className={clsx(
                     "flex", "items-center", "justify-center", "rounded",
                     "h-10",
-                    "bg-theme-accent", "hover:bg-theme-accent-hover",
+                    "bg-colour-accent", "hover:bg-colour-accent-hover",
                     "text-size-7", "text-gray-800", "hover:text-gray-900",
                     "transitions",
                 )} type="submit" aria-disabled={pending}>
